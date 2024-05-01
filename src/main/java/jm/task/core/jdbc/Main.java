060b26b718e8828e1f5a177dcd26e9f39e4c6a55
AlexanderChernuhin <alexander_963@bk.ru>
2024-05-01 13:18:46 +0300

    PP

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl();
+
+        userService.createUsersTable();
+
+        userService.saveUser("Alexander", "Chernuhin", (byte) 25);
+        userService.saveUser("Sarra", "Avraama", (byte) 120);
+        userService.saveUser("Jesus", "God's Son", (byte) 33);
+        userService.saveUser("Avraam", "Father of many", (byte) 175);
+
+
+        System.out.println(userService.getAllUsers());
+        userService.getAllUsers();
+        userService.dropUsersTable();
+
     }
 }
